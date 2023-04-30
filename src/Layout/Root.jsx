578f2885f00888de695e5792a234c3112de3603a
tofae <arnobtuhin,j@gmail.com>
2023-04-30 21:46:24 +0600

    added

---
+++ src/Layout/Root.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../Pages/Header';
+
+const Root = () => {
+    return (
+        <div>
+            <Header></Header>
+        </div>
+    );
+};
+
+export default Root;
